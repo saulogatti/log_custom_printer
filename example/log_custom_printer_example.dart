984eace9e7963b2846f850a29ef311a5e4c6f589
Saulo Gatti <saulogatti@gmail.com>
2025-10-12 01:36:19 -0300

    Initial commit

---
+++ example/log_custom_printer_example.dart
@@ -0,0 +1,6 @@
+import 'package:log_custom_printer/log_custom_printer.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ test/log_custom_printer_test.dart
@@ -0,0 +1,16 @@
+import 'package:log_custom_printer/log_custom_printer.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
